893867d2fe81922003e188b7a5871ccd2708dc10
Jorghino <jsawahn@web.de>
2015-03-03 19:25:42 +0100

    Revert d254525..07dbb35
    This rolls back to commit d254525f19774d77d5229c86947f667bfbe3c9ab.

--- index.php
+++ index.php
@@ -2,10 +2,9 @@
 	
 	require_once 'myforum-functions.php';
         $db = new mydb();
+        
        
         $db->close();
-	$db->close();
-	include "includes/promo.inc.php";
 	include "includes/promo.inc.php";
 	include "includes/footer.inc.php";
 ?>
