a08a7df51ac2dae208aeb5d34497b150cffbdf1d
Jorghino <jsawahn@web.de>
2015-03-03 19:11:07 +0100

    Test_02_zweites include promo

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 	
 	require_once 'myforum-functions.php';
         $db = new mydb();
-        
        
         $db->close();
+	include "includes/promo.inc.php";
 	include "includes/promo.inc.php";
 	include "includes/footer.inc.php";
 ?>
